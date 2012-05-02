eb3c044058ee84d0f59575937578c2637466181a
Cameron Lopez <cameron@cameron-macbook.local>
2012-05-02 14:50:19 -0500

    Fix ActionMailer

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,6 +36,6 @@ Ambassadors::Application.configure do
   config.active_support.deprecation = :stderr
   
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'utsaambassadors.org' }
 
 end
