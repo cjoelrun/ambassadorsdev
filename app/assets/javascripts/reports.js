b3f819acb5ec707077033c90ad995eb2478abad7
Cameron Lopez <cameron@cameron-macbook.local>
2012-09-25 09:53:41 -0500

    add deselect to user select in reports

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  $('.chzn-select').chosen();
-  
+  $('.chzn-select').chosen({allow_single_deselect:true});
+		      
   $("#q_roles_id_in_chzn").class = "input";
 });
+        
