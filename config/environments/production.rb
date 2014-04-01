ccee318ed03036b3d525e34e0c7a5da13c365487
cjoelrun <cameronjlopez@gmail.com>
2014-04-01 18:47:01 -0500

    ssl production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,6 @@
+require 'tlsmail'
+Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+
 Ambassadors::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
@@ -77,6 +80,7 @@ Ambassadors::Application.configure do
     from: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     enable_starttls_auto: true,
+    ssl: true,
     authentication: :plain
   }
 
